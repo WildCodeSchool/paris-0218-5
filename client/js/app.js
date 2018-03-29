02fc7c03128ffd040eacf29b5ad1d718bedbdad0
Frederic Olive <Olivefdev@gmail.com>
2018-03-29 23:52:56 +0200

    import et export insertion du json en html avec le fetch pour 1categories

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,8 @@
+import { categoriesElement } from './composants/categories.js'
+
 fetch('http://localhost:3333/catego')
     .then(res => res.json())
     .then(catego => {
         const categoElement = document.getElementById('categories')
-        categoElement.innerHTML = JSON.stringify(catego)
+        categoElement.innerHTML = catego.map(categoriesElement).join('')
     })
