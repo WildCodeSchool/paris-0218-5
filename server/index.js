c73fd371a3a833ee985ba551b14a0faaa6204ab7
Frederic Olive <Olivefdev@gmail.com>
2018-03-29 21:04:40 +0200

    create express categories route

--- server/index.js
+++ server/index.js
@@ -5,8 +5,12 @@ const catego = [categorie1]
 
 const app = express()
 
-app.get('/', (request,response)=> {
+app.get('/', (request, response)=> {
     response.send('ok')
+})
+
+app.get('/catego', (request, response) => {
+    response.json(catego)
 })
 
 app.listen(3333, () => console.log("jecoute sur le port 3333"))
