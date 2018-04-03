2d158ffe1df5dc2cfe4922f35ae74599fe40a448
SophieMdl <sophie.michaudel@gmail.com>
2018-04-03 10:26:30 +0200

    rangement

--- client/js/composants/categories.js
+++ client/js/composants/categories.js
@@ -1,6 +1,6 @@
 export const categoriesElement = mycatego => {
     return `
-			<div class='category column column-33'>
+			<div class='category column column-md-33 column-sm-50'>
 					<h4>${mycatego.titre}</h4>
 					<img src= '${mycatego.url}'>
 			</div>
