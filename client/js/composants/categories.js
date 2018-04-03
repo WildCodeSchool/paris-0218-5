d8ef2cac9c444c679ee6b4816b8985b9253e3cd4
SophieMdl <sophie.michaudel@gmail.com>
2018-04-03 10:40:27 +0200

    ajustements responsive

--- client/js/composants/categories.js
+++ client/js/composants/categories.js
@@ -1,6 +1,6 @@
 export const categoriesElement = mycatego => {
     return `
-			<div class='category column column-md-33 column-sm-50'>
+			<div class='category column column-sm-33 column-xs-100'>
 					<h4>${mycatego.titre}</h4>
 					<img src= '${mycatego.url}'>
 			</div>
