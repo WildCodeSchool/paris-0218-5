43859f1f19284264d0d444e0028950b91c7033a2
Frederic Olive <Olivefdev@gmail.com>
2018-04-10 15:34:04 +0200

    css image et button ok

--- client/js/composants/categories.js
+++ client/js/composants/categories.js
@@ -2,7 +2,7 @@ export const categoriesElement = mycatego => {
     return `
 			<div class='category column column-sm-33 column-xs-100'>
 					<h4>${mycatego.titre}</h4>
-					<img src='${mycatego.url}'>
+					<div class="image-wrapper"><img src='${mycatego.url}'></div>
 			</div>
     `
 }
