c4158d436aa00f25c40c7a76a83686d7ff730dfc
SophieMdl <sophie.michaudel@gmail.com>
2018-04-18 14:28:21 +0200

    save

--- client/js/composants/budgets.js
+++ client/js/composants/budgets.js
@@ -1,7 +1,9 @@
 export const budgetElement = mybudget => {
+  let myBudget = mybudget.name.replace('et', '-').replace(/[a-zA-Z]/g, '').trim()
+  console.log(myBudget)
   return `
   <div class='category column column-sm-33 column-xs-100'>
-    <a href="list-restaurants.html?budget=${mybudget.title}">
+    <a href="list-restaurants.html?budget=${myBudget}">
       <h4>${mybudget.name}</h4>
         <div class="image-wrapper">
             <img src='${mybudget.url}' alt="image-${mybudget.title}">
