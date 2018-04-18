13b88f13d8663197cb196627cd7a2b679739d7ff
Capucine Leclerc <capucine_49@hotmail.com>
2018-04-18 11:38:00 +0200

    modifs suite au passage de lint

--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ const util = require('util')
 const writeFile = util.promisify(fs.writeFile)
 const readFile = util.promisify(fs.readFile)
 
-
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
@@ -24,7 +23,7 @@ app.use((request, response, next) => {
   next()
 })
 
-//middleware pour gerer la requete post du formulaire
+// middleware pour gerer la requete post du formulaire
 app.use((request, response, next) => {
   if (request.method === 'GET') return next()
   let accumulator = ''
@@ -35,15 +34,13 @@ app.use((request, response, next) => {
 
   request.on('end', () => {
     try {
-    request.body = JSON.parse(accumulator)
-    next()
+      request.body = JSON.parse(accumulator)
+      next()
     } catch (err) {
       next(err)
     }
-
   })
 })
-
 
 // routes
 app.get('/', (request, response) => {
@@ -65,47 +62,31 @@ app.post('/restaurants', (request, response, next) => {
   const id = Math.random().toString(36).slice(2).padEnd(11, '0')
   const filePath = path.join(__dirname, '../mocks/restos.json')
 
-  const content = {
-    id: id,
-    name: request.body.name,
-   location: request.body.location,
-    category: request.body.category,
-    budget: request.body.budget,
-    //file
-    description: request.body.description,
-    cart: request.body.cart,
-    vegetarian: request.body.vegetarian
-  }
+  // ecrire danbs le JSON :
+  // 1 Lire le fichier et convertir le buffer en string (utf8)
+  // 2 convertir la string en objet JS
+  // 3 ajouter le nouveau bloc en array
+  // 4 convertir l'array en string
+  // 5 écrire le fichier
 
-// ecrire danbs le JSON :
-// 1 Lire le fichier et convertir le buffer en string (utf8)
-// 2 convertir la string en objet JS
-// 3 ajouter le nouveau bloc en array
-// 4 convertir l'array en string
-// 5 écrire le fichier
-
-
-// 1 Lire le fichier et convertir le buffer en string (utf8)
+  // 1 Lire le fichier et convertir le buffer en string (utf8)
   readFile(filePath, 'utf8')
 
-// 2 convertir la string en objet JS
+    // 2 convertir la string en objet JS
 
     .then(JSON.parse)
     .then(restaurants => {
-
-// 3 ajouter le nouveau bloc en array
-
+      // 3 ajouter le nouveau bloc en array
       restaurants.push({
         id: id,
         name: request.body.name,
         location: request.body.location,
         category: request.body.category,
         budget: request.body.budget,
-        //file
+        // file
         description: request.body.description,
         cart: request.body.cart,
         vegetarian: request.body.vegetarian
-
       })
 
       // 4 convertir l'array en string
@@ -113,16 +94,13 @@ app.post('/restaurants', (request, response, next) => {
 
       // 5 écrire dans le fichier
       return writeFile(filePath, content, 'utf8')
-
     })
 
     .then(() => response.end('OK'))
 
     // le catch permet de montrer l'erreur s'il y en a une
     .catch(next)
-
 })
-
 
 // port ecouter
 app.listen(3333, () => console.log('jecoute sur le port 3333'))
