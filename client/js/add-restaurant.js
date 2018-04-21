db91b9ef85bd7088aba1f61b6e0cc422f1918a2e
Capucine Leclerc <capucine_49@hotmail.com>
2018-04-21 19:22:48 +0200

    traitement formulaire ajout restos

--- client/js/add-restaurant.js
+++ client/js/add-restaurant.js
@@ -1,19 +1,46 @@
 // import { addtEtab } from './composants/addrest.js'
+const isChecked = (checkBox) => {
+  if (checkBox === true) {
+    return "Oui"
+  }
+  return "Non"
+}
+
+const nameFirstLowerCase = (nameRest) => {
+  const listWords = nameRest.split(" ") // les mots de la phrase se transforment en liste
+  const maj = (cel) => { // chaque premiere lettre de chaque élément est mis en majuscule
+    const wordMaj = cel.charAt(0).toUpperCase() + cel.substring(1).toLowerCase()
+    return wordMaj
+  }
+  const listWordsMaj = listWords.map(maj)
+  const nameRestMaj = listWordsMaj.join(" "); // le tableau est transformé en phrase dont les mots sont séparés par des espaces
+  return nameRestMaj
+}
 
 const form = document.getElementById('form')
 
 form.addEventListener('submit', event => {
   event.preventDefault()
-  console.log('toto')
 
-  const name = document.getElementById('name-etab').value
-  const location = document.getElementById('adress-etab').value
+  let name = document.getElementById('name-etab').value
+  let location = document.getElementById('adress-etab').value
   const category = document.getElementById('catego-etab').value
-  // const url = ''
+  let url = document.getElementById('file').value
   const budget = document.getElementById('budget-etab').value
   const description = document.getElementById('description-etab').value
-  const cart = document.getElementById('cb-etab').value
-  const vegetarian = document.getElementById('vege-etab').value
+  let cart = document.getElementById('cb-etab').checked
+  let vegetarian = document.getElementById('vege-etab').checked
+  let takeAway = document.getElementById('away-etab').checked
+
+  if (url === '') {
+    url = `images/categories/${category.toLowerCase()}.jpg`
+  }
+
+cart = isChecked(cart)
+vegetarian = isChecked(vegetarian)
+takeAway = isChecked(takeAway)
+name = nameFirstLowerCase(name)
+location = nameFirstLowerCase(location)
 
   window.fetch('http://localhost:3333/restaurants', {
     method: 'post',
@@ -21,7 +48,7 @@ form.addEventListener('submit', event => {
       name: name,
       location: location,
       category: category,
-      // url: url,
+      url: url,
       budget: budget,
       description: description,
       cart: cart,
