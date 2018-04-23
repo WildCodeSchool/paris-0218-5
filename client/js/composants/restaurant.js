1ac0446e70b8db7ec0d66db371cddc4c4495070d
SophieMdl <sophie.michaudel@gmail.com>
2018-04-23 11:05:11 +0200

    début inté

--- client/js/composants/restaurant.js
+++ client/js/composants/restaurant.js
@@ -1,18 +1,19 @@
 export const restaurantElement = restaurant => {
+  let str = `
+  <div class='column column-md-33 column-sm-50 xs-100'>
+  <div class="restaurant">
+    <img src="${restaurant.url}">
+    <h5>${restaurant.name}</h5>
+    <h6>${restaurant.category}</h6>
+    <p class="more-infos">${restaurant.description}</p>
+    <span class="icn icn-like"></span>${restaurant.like.length}
+    <p>${restaurant.budget}</p>
+  </div>
+</div>
+  `
+  if()
   return `
-      <div class='column column-md-33 column-sm-50 xs-100'>
-        <div class="restaurant">
-          <img src="${restaurant.url}">
-          <h5>${restaurant.name}</h5>
-          <h6>${restaurant.category}</h6>
-          <p class="more-infos">${restaurant.description}</p>
-          <p class="more-infos">A emporter ?</p>
-          <p class="more-infos">Végétarien ? ${restaurant.vegetarian}</p>
-          <p class="more-infos">Carte bancaire ? ${restaurant.cart}</p>
-          <p class="more-infos">${restaurant.location}</p>
-          <span class="icn icn-like"></span>${restaurant.like.length}
-          <p>${restaurant.budget}</p>
-        </div>
-    </div>
+
     `
+
 }
