21aee70ed5619a3edaef53b18cd399272cc7c20e
Capucine Leclerc <capucine_49@hotmail.com>
2018-04-30 18:13:22 +0200

    traitement formulaire

--- client/js/add-restaurant.js
+++ client/js/add-restaurant.js
@@ -1,82 +1,45 @@
-        // import { addtEtab } from './composants/addrest.js'
-        const isChecked = (checkBox) => {
-          if (checkBox === true) {
-              return 'Oui'
-          }
-          return 'Non'
-      }
-
-      const nameFirstLowerCase = (nameRest) => {
-          const listWords = nameRest.split(' ') // les mots de la phrase se transforment en liste
-          const maj = (cel) => { // chaque premiere lettre de chaque élément est mis en majuscule
-              const wordMaj = cel.charAt(0).toUpperCase() + cel.substring(1).toLowerCase()
-              return wordMaj
-          }
-          const listWordsMaj = listWords.map(maj)
-          const nameRestMaj = listWordsMaj.join(' ')
-          return nameRestMaj
-      }
+// import { addtEtab } from './composants/addrest.js'
 
 
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  const formTarget = event.target
 
+  // Objet special pour le traitement des formulaires : formData
+  const data = new FormData(formTarget)
 
-      form.addEventListener('submit', event => {
-          event.preventDefault()
-          const formTarget = event.target
+  // On accede aux éléments de cet objet avec la méthode GET
+  const isChecked = (item) => {
+    if (data.get(item) === 'on') {
+      return data.set(item, 'Oui')
+    }
+  }
 
-          // En suite en JS on à un objet spécial pour traiter les formulaire
-          // FormData
+  const nameFirstLowerCase = (item, valueGet) => {
+    const listWords = valueGet.split(' ') // les mots de la phrase se transforment en liste
+    const maj = (cel) => { // chaque premiere lettre de chaque élément est mis en majuscule
+      const wordMaj = cel.charAt(0).toUpperCase() + cel.substring(1).toLowerCase()
+      return wordMaj
+    }
+    const listWordsMaj = listWords.map(maj)
+    const nameRestMaj = listWordsMaj.join(' ')
+    return data.set(item, nameRestMaj)
+  }
 
-          // On l'obtient à partir d'un form :
-          const data = new FormData(formTarget)
+  isChecked('cart')
+  isChecked('vegetarian')
+  isChecked('takeAway')
 
-          /* if (data.get('url') === '')
-          {data.append("url", `images/categories/${category.toLowerCase()}.jpg`)
+  const nameValue = data.get('name')
+  const locValue = data.get('location')
 
-          }*/
+  nameFirstLowerCase('name', nameValue)
+  nameFirstLowerCase('location', locValue)
 
-          // C'est un objet un peu chelou, j'ai aucune idée de pourquoi c'est pas un
-          // objet normal, c'est le DOM, on pause pas de question.
-
-          // Donc la différence c'est que pour acceder au elements on peu pas juste
-          // faire avec le . ou les []
-          // il faut utilisé la méthode `get` :
-
-
-          // Il va reprendre chaque <input> de votre <form>
-          // et il prend leur propriété "name" comme key
-          // et la prop "value" comme valeur
-
-
-
-
-      let name = document.getElementById('name-etab').value
-      let location = document.getElementById('adress-etab').value
-      const category = document.getElementById('catego-etab').value
-      let url = document.getElementById('fileup').value
-      const budget = document.getElementById('budget-etab').value
-      const description = document.getElementById('description-etab').value
-      let cart = document.getElementById('cb-etab').checked
-      let vegetarian = document.getElementById('vege-etab').checked
-      let takeAway = document.getElementById('away-etab').checked
-
-       if (url === '') {
-         url = `images/categories/${category.toLowerCase()}.jpg`
-       }
-
-      cart = isChecked(cart)
-      vegetarian = isChecked(vegetarian)
-      takeAway = isChecked(takeAway)
-      name = nameFirstLowerCase(name)
-      location = nameFirstLowerCase(location)
-
-          // avec fetch ça s'utilise comme ça:
-          fetch('http://localhost:3333/restaurants', { method: 'post', body: data })
-          .then(response => response.json())
-          .then(res => console.log(data.get('url')))
-          .then(res => console.log(res))
-          .catch(err => console.log(err))
-          .then(res => window.location.replace('index.html'))
-
-      })
+  fetch('http://localhost:3333/restaurants', { method: 'post', body: data })
+    .then(response => response.json())
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+    .then(res => window.location.replace('index.html'))
+})
 
