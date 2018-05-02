00a797aedbdb15d4bb3bab0bda792a19d274d8b1
Capucine Leclerc <capucine_49@hotmail.com>
2018-05-02 11:13:08 +0200

    modif disabled sur boutons$

--- client/js/profil.js
+++ client/js/profil.js
@@ -11,11 +11,36 @@ window.fetch(`http://localhost:3333/profil/${id}`)
     profilElement.innerHTML = profilPage(users)
   })
   .then(() => {
+    const validate = document.getElementById('valid')
+    validate.disabled = true
     const modify = document.getElementById('modify')
     modify.addEventListener('click', () => {
       const inputs = profilElement.querySelectorAll('input')
       for (let input of inputs) {
         input.removeAttribute('disabled')
       }
+      modify.disabled = true
+      validate.disabled = false
+
     })
   })
+
+/*
+  const validate = document.getElementById('valid')
+
+  /* ****** GESTION MODAL *******
+export const scriptPopup = () => {
+  const getModal = document.getElementById('open-popup')
+  const showModal = document.getElementById('popup-cnx')
+
+  getModal.addEventListener('click', () => {
+    showModal.style.display = 'block'
+    const closePopup = document.getElementById('close-popup')
+    closePopup.addEventListener('click', () => {
+      showModal.style.display = 'none'
+    })
+  })
+
+  quand on clic sur modifyle bouton modify = disabled
+  le bouton valider = able
+*/
