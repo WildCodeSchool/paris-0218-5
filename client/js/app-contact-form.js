17d34416b2ea1d68004668149561de6b60beff6b
Kim Spychalski <kim.spychalski@gmail.com>
2018-05-03 15:36:10 +0200

    ajout fichiers de code pour faire fonctionner formulaire contact

---
+++ client/js/app-contact-form.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const nodemailer = require('nodemailer');
+const contact = require('./js/contact-form.js')
+const app = express();
+
+
+
+// Static folder (public)
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+  })
+
+app.get('/', (req, res) => {
+    res.send('contact')
+});
+
+app.post('/send', contact)
+
+app.listen(3000, () => console.log('Server started...3000'));
+
+
