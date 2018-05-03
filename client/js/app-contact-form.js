673496f1d82a66c82f35f79ac48b53e485102611
Kim Spychalski <kim.spychalski@gmail.com>
2018-05-03 18:02:51 +0200

    corrections travis

--- client/js/app-contact-form.js
+++ client/js/app-contact-form.js
@@ -1,31 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const nodemailer = require('nodemailer');
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const nodemailer = require('nodemailer')
 const contact = require('./js/contact-form.js')
-const app = express();
-
-
+const app = express()
 
 // Static folder (public)
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 // Body Parser Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
   })
 
 app.get('/', (req, res) => {
-    res.send('contact')
-});
+  res.send('contact')
+})
 
 app.post('/send', contact)
 
-app.listen(3000, () => console.log('Server started...3000'));
-
-
+app.listen(3000, () => console.log('Server started...3000'))
--- client/js/contact-form.js
+++ client/js/contact-form.js
@@ -5,8 +5,8 @@ const nodemailer = require('nodemailer')
 const bodyParser = require('body-parser')
 
 // Body Parser Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // app.engine('html', require('ejs').renderFile);
 // app.set('view engine', 'html');
@@ -15,14 +15,14 @@ console.log('je me charge')
 
 // creer la route send
 router.get('/send', (req, res, next) => {
-    console.log('jesuis la')
-    res.send('ok')
-    // on cible le formulaire et on cible l'event (submit)
-    const form = window.getElementById('contact-form')
-    console.log(form)
-     form.addEventListener('submit', event => {
-         // variable qui contient les valeurs rentree par l'users
-         const contactContent = `
+  console.log('jesuis la')
+  res.send('ok')
+  // on cible le formulaire et on cible l'event (submit)
+  const form = window.getElementById('contact-form')
+  console.log(form)
+  form.addEventListener('submit', event => {
+    // variable qui contient les valeurs rentree par l'users
+    const contactContent = `
              <p>Un utilisateur vous a écrit.</p>
              <ul>
                  <li>Name: ${req.body.name}</li>
@@ -32,32 +32,30 @@ router.get('/send', (req, res, next) => {
                  <li>Telephone: ${req.body.telnum}</li>
              </ul>
          `
-         // cree le transporter
-         const transporter = nodemailer.createTransport({
-             host: 'smtp.ethereal.email',
-             port: 587,
-             auth: {
-                 user: 'jt3vfgego6giu4ym@ethereal.email',
-                 pass: 'Fzvhh4KQbpBbw8N2Ut'
-             }
-         })
-         // Mise en forme du mail 
-         const mailOptions = {
-             from: ` "${req.body.name}" <${req.body.mail}> `,
-             to: 'kim.spychalski@gmail.com',
-             subject: `${req.body.reason} `,
-             text: `${req.body}`,
-             html: contactContent
+    // cree le transporter
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+          user: 'jt3vfgego6giu4ym@ethereal.email',
+          pass: 'Fzvhh4KQbpBbw8N2Ut'
+        }
+    })
+    // Mise en forme du mail
+    const mailOptions = {
+      from: ` "${req.body.name}" <${req.body.mail}> `,
+      to: 'kim.spychalski@gmail.com',
+      subject: `${req.body.reason} `,
+      text: `${req.body}`,
+      html: contactContent
 
-         }
-         // on gere l'erreur 
-         transporter.sendMail(mailOptions, (err, data => {
-
-             if(err) {
-                 return console.log(err)
-             }
-
-             console.log(data)
+    }
+    // on gere l'erreur
+    transporter.sendMail(mailOptions, (err, data => {
+             if (err) {
+      return console.log(err)
+      }
+      console.log(data)
          }))
      })
 })
